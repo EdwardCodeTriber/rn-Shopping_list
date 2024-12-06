f8a79cc1e7274c88ed2b959d57a09b856bad9bee
Thapelo Somo <edwardthapelo55@gmail.com>
2024-12-06 11:09:31 +0200

    Moved the default categories to the created categorySlice. created a itemsSlice to add items according to their category and created the shopstore.

--- shop-app/App.jsx
+++ shop-app/App.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './Redux/Store';
-import ShoppingListScreen from './screens/ShoppingListScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { store, persistor } from './Redux/shopstore';
 import ShoppingList from './screens/ShoppingList';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import HouseholdScreen from './screens/HouseholdList';
+import ItemsList from './screens/ItemsList'; 
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    // <Provider store={store}>
-    //   <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaView style={styles.container}>
-           {/* <ShoppingListScreen />  */}
-            <ShoppingList/>
-            <HouseholdScreen />
-        </SafeAreaView>
-    //   </PersistGate>
-    // </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#2D2E37', // Change this to your desired color
+              },
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                textAlign: 'center',
+                alignSelf: 'center',
+              },
+              headerTintColor: '#ffffff', // Color of the back button and title
+              headerTitleAlign: 'center', // This ensures the title is centered
+            }}
+          >
+            <Stack.Screen 
+              name="Categories" 
+              component={ShoppingList} 
+              options={{ 
+                title: 'Shopping Categories',
+              }}
+            />
+            <Stack.Screen 
+              name="ItemsList" 
+              component={ItemsList} 
+              options={({ route }) => ({ 
+                title: `${route.params.category.name} Items` 
+              })}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-});
